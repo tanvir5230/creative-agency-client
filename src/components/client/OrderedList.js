9a0d932b6cf676bcb8b6cafff5b1d2567a8e3069
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-10-16 22:44:39 +0600

    orderlist beautified

--- src/components/client/OrderedList.js
+++ src/components/client/OrderedList.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Axios from "axios";
 import Loader from "../loader/Loader";
-import { Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import { userContext } from "../../App";
 export const OrderedList = ({ setTitle }) => {
   const { url } = useContext(userContext);
@@ -18,28 +18,40 @@ export const OrderedList = ({ setTitle }) => {
     });
   }, [newTitle, setTitle, uid, url]);
   return (
-    <div>
+    <Row>
       {list === null && <Loader />}
       {list && list.length === 0 && <p>You have no order.</p>}
       {list &&
         list.length > 0 &&
         list.map((item) => {
           return (
-            <Col xs={12} md={6} lg={4}>
-              <div className="h-100 border px-3 py-2">
-                <p>{item.orderedService}</p>
+            <Col xs={12} md={6} lg={4} className="p-3">
+              <div className="h-100 border p-3">
+                <p className="font-weight-bold text-center">
+                  {item.orderedService}
+                </p>
                 {item.status === "pending" && (
-                  <span className="bg-danger p-2 text-white">
-                    {item.status}
-                  </span>
+                  <p className="p-2 text-danger d-flex justify-content-center">
+                    <i className="fa fa-hourglass-1 fa-spin fa-2x"></i>
+                    <span className="ml-3">{item.status}</span>
+                  </p>
                 )}
                 {item.status === "done" && (
-                  <span className="btn btn-success">{item.status}</span>
+                  <p className="p-2 text-danger d-flex justify-content-center">
+                    <i className="fa fa-check-circle-o fa-2x"></i>
+                    <span className="ml-3">{item.status}</span>
+                  </p>
+                )}
+                {item.status === "on Going" && (
+                  <p className="p-2 text-warning d-flex justify-content-center">
+                    <i className="fa fa-spinner fa-spin fa-2x"></i>
+                    <span className="ml-3">{item.status}</span>
+                  </p>
                 )}
               </div>
             </Col>
           );
         })}
-    </div>
+    </Row>
   );
 };
